2b7a25fe54f0948bf1dc2d15241fda6c4132bcb4
Katherine Hébert <katherine.ahebert@gmail.com>
2021-06-22 10:57:26 -0400

    initial commit

---
+++ Katherine/skeleton/app.R
@@ -0,0 +1,50 @@
+### ----------------------------------------------
+### ----------------------------------------------
+### This is an example of a simple Shiny app which
+### makes a figure that is customisable based on
+### some user input(s)
+### ----------------------------------------------
+### ----------------------------------------------
+
+# PACKAGES & DATA PREP: Source tabs --------------------------------------------
+# source the script that uploads and cleans data, and loads packages
+source("00_initialize_app.R")
+
+
+# USER INTERFACE (how the app looks) -------------------------------------------
+
+ui <- fluidPage(
+
+    # Application title ----
+    titlePanel("Exploring volcano explosivity"),
+
+
+    # Input interface ----
+
+            sliderInput("years", # this is important! it's the id your server needs to use the selected value
+                        label = h3("Years"),
+                        min = 1900, max = 2020, # maximum range that can be selected
+                        value = c(2010, 2020) # this is the default slider position
+            ),
+
+        # Show the outputs from the server ---------------
+        mainPanel(
+            
+          # print out the years that were chosen
+            verbatimTextOutput("years") 
+            
+        )
+    )
+
+
+# SERVER (how the app works) ---------------------------------------------------
+# Define server logic required to make your output(s)
+
+server <- function(input, output) {
+
+    output$years <- renderPrint({input$years}) 
+    
+}
+
+# Run the application
+shinyApp(ui = ui, server = server)
